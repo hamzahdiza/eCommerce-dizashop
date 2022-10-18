e876e3e1cc17db63ea2653e7d9eb4ae0d13758b8
Hamzah <hamzah.diza@gmail.com>
2022-10-18 23:16:16 +0700

    Update USer Register Reducer, Action, & Screen

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -4,6 +4,7 @@ import MainApp from "../MainApp";
 import HomePage from "../page/HomePage";
 import ProductPage from "../page/ProductPage";
 import CartPage from "../page/CartPage";
+import LoginPage from "../page/LoginPage";
 
 const Routes = () => {
   return (
@@ -11,12 +12,13 @@ const Routes = () => {
       <Router>
         <Switch>
           <Route path="/" element={<MainApp />}>
-            <Route path="/" element={<HomePage />} exact />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/product/:id" element={<ProductPage />} />
             <Route path="/cart" element={<CartPage />}>
               <Route path=":id" element={<CartPage />} />
               <Route path="" element={<CartPage />} />
             </Route>
+            <Route path="/" element={<HomePage />} exact />
           </Route>
         </Switch>
       </Router>
