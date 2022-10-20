5fa643fb974d01392fe20c377bc271015b7fcc92
Hamzah <hamzah.diza@gmail.com>
2022-10-20 14:18:29 +0700

    update Get order by Id & Order datils reducer and action

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -31,4 +31,18 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc   Get Order By ID
+// @route  Get /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate("user", "name email");
+
+  if (order && (req.user.isAdmin || order.user._id.equals(req.user._id))) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
--- frontend/src/page/PlaceOrderPage.js
+++ frontend/src/page/PlaceOrderPage.js
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import CheckoutSteps from "../components/CheckoutSteps";
+import { createOrder } from "../actions/orderActions";
+// import { ORDER_CREATE_RESET } from "../constants/orderConstants";
+// import { USER_DETAILS_RESET } from "../constants/userConstants";
 
 const PlaceOrderPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const cart = useSelector((state) => state.cart);
 
   // Calculate Prices
@@ -19,8 +25,29 @@ const PlaceOrderPage = () => {
   cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
   cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2);
 
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(() => {
+    if (success) {
+      navigate(`/order/${order._id}`);
+      // dispatch({ type: USER_DETAILS_RESET });
+      // dispatch({ type: ORDER_CREATE_RESET });
+    }
+    // eslint-disable-next-line
+  }, [dispatch, success]);
+
   const placeOrderHandler = () => {
-    console.log("");
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
 
   return (
@@ -100,7 +127,7 @@ const PlaceOrderPage = () => {
                   <Col>${cart.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {/* <ListGroup.Item>{error && <Message variant="danger">{error}</Message>}</ListGroup.Item> */}
+              <ListGroup.Item>{error && <Message variant="danger">{error}</Message>}</ListGroup.Item>
               <ListGroup.Item>
                 <Button type="button" className="btn-block" disabled={cart.cartItems === 0} onClick={placeOrderHandler}>
                   Place Order
