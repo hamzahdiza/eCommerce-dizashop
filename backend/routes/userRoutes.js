86e560a0414ecfab3dcb921735add55cfbb83ae4
Hamzah <hamzah.diza@gmail.com>
2022-10-25 01:58:13 +0700

    Update Admin, Get user by ID & Update user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 // import asyncHandler from "express-async-handler";
 // import Product from "../models/productModel.js";
 // import mongoose from "mongoose";
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser } from "../controllers/userControllers.js";
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from "../controllers/userControllers.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -10,6 +10,6 @@ const router = express.Router();
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route("/:id").delete(protect, admin, deleteUser);
+router.route("/:id").delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser);
 
 export default router;
