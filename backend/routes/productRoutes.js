42680790847b097230a2bc0a732b1e31bddfe06b
Hamzah <hamzah.diza@gmail.com>
2022-10-27 23:26:43 +0700

    Morgan & create review endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 // import asyncHandler from "express-async-handler";
 // import Product from "../models/productModel.js";
 // import mongoose from "mongoose";
-import { getProducts, getProductById, deleteProduct, createProduct, updateProduct } from "../controllers/productControllers.js";
+import { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview } from "../controllers/productControllers.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
 
 export default router;
